df63a01902c020194460f6d3c0560df1294d8c49
Gauna Somá, Facundo <99784140+facu-gs@users.noreply.github.com>
2024-08-16 04:45:13 +0000

    Added some comments

--- tour-service/src/main/java/com/example/Application.java
+++ tour-service/src/main/java/com/example/Application.java
@@ -1,5 +1,6 @@
 package com.example;
 
+/* This interface represents the Spring IoC container and is responsible for instantiating, configuring, and assembling the beans */
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -9,8 +10,16 @@ import com.example.tourservice.TravelAgentService;
 public class Application {
 
     public static void main(String[] args) {
+        /* Instantiate the application context */
         ApplicationContext context = new AnnotationConfigApplicationContext(SpringBeans.class);
+        /*
+         * You use .class when you need to reference the Class object without creating
+         * an instance of the class
+         * The Class Object it is used to hold metadata about a class
+         */
         TravelAgentService agent = context.getBean(TravelAgentService.class);
+
+        
         System.out.println("\n******Explore California Tour Catalogue******");
         agent.displayTours();
 
--- tour-service/src/main/java/com/example/tourservice/SpringBeans.java
+++ tour-service/src/main/java/com/example/tourservice/SpringBeans.java
@@ -1,17 +1,30 @@
 package com.example.tourservice;
 
+/* Here we have the configuration for our dependendy injection */
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.example.tourservice.utilities.TourRepository;
 
+/* 
+* @Configuration is a class-level annotation indicating 
+* that an object is a source of bean definitions.  
+*/
+
 @Configuration
 public class SpringBeans {
- 
+
+  /*
+   * The @Bean annotation is used to indicate that a method instantiates,
+   * configures, and initializes a new object to be managed by the Spring IoC
+   * container.
+   */
   @Bean
   public TourRepository tourRepository() {
     return new TourRepository();
   }
+
   @Bean
   public TourManagementService tourManagementService() {
     return new TourManagementService(tourRepository());
@@ -20,5 +33,5 @@ public class SpringBeans {
   @Bean
   public TravelAgentService travelAgentService() {
     return new TravelAgentService(tourRepository());
-  } 
+  }
 }
